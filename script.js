7d488663c836ca92cd5053fbd5fc638a2342e49b
Jude Evroh <35838851+ehvy-jd@users.noreply.github.com>
2022-09-27 15:44:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let notice = document.getElementById("notice");
+let noticeBtn = document.getElementById("notice-button");
+let ft = document.getElementById("foot");
 
 const remove = (notice) => {
   notice.remove();
 };
 
+const footHeight = (ft) => {
+  ft.style.height = "auto";
+};
 
 

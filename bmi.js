d7862ff891d990adf182fa72d87a22733122cb33
Jude Evroh <35838851+ehvy-jd@users.noreply.github.com>
2022-09-30 14:28:10 +0100

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,63 @@
+// Bmi calculator
+let bmiSubmit = document.getElementById("bmi-submit");
+let bmiDiv = document.getElementById("bmi-div");
+let bmiResult = document.getElementById("bmi-result");
+let reset = document.getElementById("bmi-reset");
+
+let calculateBmi = () => {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+  let bmi = (weight / (height / 100) ** 2).toFixed(2);
+
+  let superScript = "<sup>2</sup>";
+
+  if (bmi < 18.5) {
+    bmiDiv.style.display = "block";
+    bmiResult.innerHTML =
+      "Your BMI is " +
+      bmi +
+      "kg/m" +
+      superScript +
+      ".<br> According to the World Health Organisation recommendations on Body Mass Index, your bmi falls under the underweight category.";
+  }
+
+  else if (bmi >= 18.5 && bmi <= 24.9) {
+    bmiDiv.style.display = "block";
+    bmiResult.innerHTML =
+      "Your BMI is " +
+      bmi +
+      "kg/m" +
+      superScript +
+      ".<br> According to the World Health Organisation recommendations on Body Mass Index, your bmi falls under the normal weight category.";
+  }
+
+  else if (bmi >= 25 && bmi <= 29.9) {
+    bmiDiv.style.display = "block";
+    bmiResult.innerHTML =
+      "Your BMI is " +
+      bmi +
+      "kg/m" +
+      superScript +
+      ".<br> According to the World Health Organisation recommendations on Body Mass Index, your bmi falls under the Pre-Obese category.";
+  }
+
+  else {
+    bmiDiv.style.display = "block";
+    bmiResult.innerHTML =
+      "Your BMI is " +
+      bmi +
+      "kg/m" +
+      superScript +
+      ".<br> According to the World Health Organisation recommendations on Body Mass Index, your bmi falls under the Obese category.";
+  }
+};
+
+bmiSubmit.addEventListener("click", calculateBmi);
+
+const resetBmi = () => {
+  bmiDiv.style.display = "none";
+  weight.value = "";
+  height.value = "";
+};
+
+reset.addEventListener("click", resetBmi);
---
+++ main.js
@@ -0,0 +1,49 @@
+// Notification close button event handler
+let notice = document.getElementById("notice");
+let noticeBtn = document.getElementById("notice-button");
+let ft = document.getElementById("foot");
+
+const remove = (notice) => {
+  notice.remove();
+};
+
+const footHeight = (ft) => {
+  ft.style.height = "auto";
+};
+
+// Photo slide handler
+let slideIndex = 0;
+const showSlides = () => {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 3000);
+};
+showSlides();
+
+// Scroll header handler
+let scrollDown = document.getElementById("scroll-header");
+let mainHeader = document.getElementById("landing");
+window.addEventListener(
+  "scroll",
+  (showMiniHeader = () => {
+    if (window.scrollY > 10) {
+      scrollDown.style.display = "flex";
+    } else {
+      scrollDown.style.display = "none";
+    }
+  })
+);
+
